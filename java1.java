222cc58eb127aed98269c0e96cfc8ed2e4b7b2a0
1819127809 <69291822+1819127809@users.noreply.github.com>
2021-04-24 15:15:05 +0800

    Update java1.java

--- java1.java
+++ java1.java
@@ -1,4 +1,19 @@
-111
 
+public class Demo_MutiplicationTable {	
+  
+	public static void main(String[] args) {
+		
+		for(int i = 1;i <= 9;i++) {
+			for(int j = 1;j <= i;j++) {
+				if(i == j) {
+					System.out.println(i + "*" + j + "=" + i * j);
+				}else {
+					System.out.print(i + "*" + j + "=" + i * j + " ");
+				}				
+			}			
+		}		
 
-...
+  }
+  
+}
+
